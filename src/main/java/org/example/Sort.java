7722e5b13355c7c7e7a681dd3778d4d55b5714d9
ryan.alexandra <ryan.alexandra.m@gmail.com>
2022-03-23 10:40:48 -0700

    added merge and sort

--- src/main/java/org/example/Sort.java
+++ src/main/java/org/example/Sort.java
@@ -9,7 +9,14 @@ public class Sort {
      */
     public static void sortTwo(int[] nums) {
 
-    }
+            if(nums[0]> nums[1]){
+                int hold= nums[0];
+                nums[0]= nums[1];
+                nums[1]= hold;
+            }
+        }
+
+
 
     /**
      * This function should take the two arrays and merge them into the inputArray.
@@ -19,6 +26,41 @@ public class Sort {
      * @param array2 - A sorted list of numbers that is the size of half the length of the inputArray
      */
     public static void merge(int[] inputArray, int[] array1, int [] array2) {
+        //int i, j, k =0;
+        //LH= left hand
+        //RH= right hand
+        //BB= big boy list
+        int i=0;
+        int j=0;
+        int k=0;
+        //while there is still room in the LH and RH list
+        while(i<array1.length && j<array2.length){
+            //if LH[i] >RH[j]: LH[i]= BB[k]
+            if(array1[i]< array2[j]){
+                inputArray[k]= array1[i];
+                //LH++, BB++;
+                i++;
+                k++;
+            }
+            //if LH[i] <RH[j]: LH[i]= BB[k]
+            if(array1[i]> array2[j]){
+                inputArray[k]= array2[j];
+                //RH++, BB++;
+                j++;
+                k++;
+            }}
+        //if LH is longer than RH: clean out LH until the last of BB
+        while(array1.length>array2.length && k<inputArray.length){
+            inputArray[k]= array1[i];
+            i++;
+            k++;
+        }
+        //if RH is longer than LH: clean out RH until the last of BB
+        while(array2.length>array1.length && k<=inputArray.length){
+            inputArray[k]= array2[j];
+            j++;
+            k++;
+        }
 
     }
 }
